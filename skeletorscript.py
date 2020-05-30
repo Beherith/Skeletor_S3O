46b60b34929073b31383a936e2b4d6eeef03908b
Beherith <mysterme@gmail.com>
2020-05-30 22:53:20 +0200

    add bl_info

--- skeletorscript.py
+++ skeletorscript.py
@@ -1,3 +1,32 @@
+
+#!BPY
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name": "Skeletor_S3O SpringRTS (.s3o)",
+    "author": "Beherith  <mysterme@gmail.com>",
+    "version": (0, 1, 0),
+    "blender": (2, 80, 0),
+    "location": "3D View > Side panel",
+    "description": "Create a Skeleton and a BOS for a SpringRTS",
+    "warning": "",
+    "wiki_url": "https://github.com/Beherith/Skeletor_S3O",
+    "tracker_url": "http://springrts.com",
+    "support": "COMMUNITY",
+    "category": "Rigging",
+}
+
 import bpy
 from math import pi, degrees
 from mathutils import Vector, Euler, Matrix
